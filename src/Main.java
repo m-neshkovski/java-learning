081c65fb31c6e848a130ba282426b12ea8329ae6
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-27 14:29:54 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        printFinalScore(
+                calculateScore(true, 800, 5, 100)
+        );
+
+        printFinalScore(
+                calculateScore(true, 10000, 8, 200)
+        );
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+
+        if(gameOver) {
+
+            finalScore += bonus * levelCompleted;
+        }
+
+        return finalScore;
+    }
+
+    public static void printFinalScore(int finalScore) {
+        System.out.println("Your final score was: " + finalScore);
     }
 }
