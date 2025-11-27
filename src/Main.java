890303d035df03dca4e92c36a8197bff5e22b063
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-27 13:32:15 +0100

    done lesson

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 5000) {
+            // If the condition is true
+            System.out.println("Your score was 5000!");
+        } else if (score < 5000) {
+            System.out.println("Your score was less than 5000!");
+        } else {
+            System.out.println("You got here");
+        }
     }
+
 }
