2e152679fa5b43259c61b8784163be51c9453f56
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-27 09:46:04 +0100

    intermidiate commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // "myVariable = 50" is an expression.
+        // The whole line "int myVariable = 50;" is a statement
+        int myVariable = 50;
+
+        // These are all statements
+        myVariable++;
+        myVariable--;
+
+        System.out.println("My variable = " + myVariable);
+
+        System.out.println("This is a statement!");
+        System.out.println(
+            "This is " +
+            "also " +
+            "a statement " +
+            "but in multiple lines for clarity!"
+        );
+        System.out.println("This is " + "also " + "a statement " + "but in multiple lines for clarity!");
+
+        // What is a whitespace? Whitespace is a space, tab, or newline character.
+        // Makes the code more readable.
     }
 }
