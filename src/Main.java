38b0b32ffc0717454f70f136f2f0f8b5551246be
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-28 16:12:39 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(convertToCentimeters(6, 0));
+    }
+
+    // 1 inch = 2.54cm, 1 foot = 12 inches
+
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+        return convertToCentimeters(feet * 12 + inches);
     }
 }
