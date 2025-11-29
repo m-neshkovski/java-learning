0b50be22a7779493afa41748803687d0704c0646
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-29 12:06:05 +0100

    first solution

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(getDurationString(59));
+    }
+
+    public static String getDurationString(int seconds) {
+
+        if (seconds < 0) {
+            return "Invalid value for seconds";
+        }
+
+        return getDurationString(seconds / 60, seconds % 60);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+
+        if (minutes < 0) {
+            return "Invalid value for minutes";
+        }
+
+        if (seconds < 0 || seconds > 59) {
+            return "Invalid value for seconds";
+        }
+
+        return String.format("%02d", minutes / 60) + "h "
+                + String.format("%02d", minutes % 60) + "m "
+                + String.format("%02d", seconds) + "s";
     }
 }
