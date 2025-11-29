5dfebf0fc05e5cd5620f5ab8d1d947b5281c56bb
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-29 20:15:03 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+    }
+
+    public static void printDayOfWeek(int day) {
+//        String dayOfWeek = switch (day) {
+//            case 1 -> "Monday";
+//            case 2 -> "Tuesday";
+//            case 3 -> "Wednesday";
+//            case 4 -> "Thursday";
+//            case 5 -> "Friday";
+//            case 6 -> "Saturday";
+//            case 0 -> "Sunday";
+//            default -> "Invalid day";
+//        };
+
+//        System.out.println("Day " + day + " is " + dayOfWeek);
+        System.out.println("Day " + day + " is " + dayOfWeekMap(day));
+    }
+
+    public static String dayOfWeekMap(int day) {
+        return switch (day) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 0 -> "Sunday";
+            default -> "Invalid day";
+        };
     }
 }
