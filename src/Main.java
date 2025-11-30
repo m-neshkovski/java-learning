aa14ee4ff0d1e55d2a1c9e4576081275147c5a62
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-30 09:48:19 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int i = 4;
+        int evenCount = 0;
+        int oddCount = 0;
+        while (i <= 20) {
+            i++;
+            if (! isEvenNumber(i)) {
+                oddCount++;
+                continue;
+            }
+            System.out.println(i);
+            evenCount++;
+            if (evenCount >= 5) break;
+//            i++;
+        }
+        System.out.println("Even count = " + evenCount);
+        System.out.println("Odd count = " + oddCount);
+    }
+
+    public static boolean isEvenNumber(int number) {
+        return (number >= 0) && (number % 2 == 0);
     }
 }
