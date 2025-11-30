f892c4d51f4d518d18c2aa0a7535d17f8546b18c
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-30 09:00:46 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(sumOdd(1, 12));
+    }
+
+    // write code here
+    public static boolean isOdd(int number) {
+        return (number >= 0) && (number % 2 != 0);
+    }
+
+    public static int sumOdd(int start, int end) {
+        if(((start < 0) || (end < 0)) || (start > end)) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        for(int number = start; number <= end; number++) {
+            if(isOdd(number)) {
+                sum += number;
+            }
+        }
+
+        return sum;
     }
 }
