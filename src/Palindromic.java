fd36f1fd5b0948354f42963e3b8878d3d77b46a3
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-30 20:01:41 +0100

    wip

---
+++ src/Palindromic.java
@@ -0,0 +1,50 @@
+public class Palindromic {
+
+    public static void main(String[] args) {
+        if(isPalindrome(121)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isPalindrome(int x) {
+        int reverse = 0;
+        int original = x;
+        boolean isNegative = x < 0;
+
+        if(isNegative) {
+            x *= -1;
+        }
+
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (isNegative) {
+            reverse *= -1;
+        }
+        System.out.println(reverse);
+        return reverse == original;
+    }
+
+    public static int reverse(int x) {
+        int reverse = 0;
+        boolean isNegative = x < 0;
+        if (isNegative) {
+            x *= -1;
+        }
+
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (isNegative) {
+            reverse *= -1;
+        }
+
+        return reverse;
+    }
+}
