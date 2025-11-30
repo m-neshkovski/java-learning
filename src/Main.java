9e47261c8b67b5b76b9ca8923e6c7b5c281b1744
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-11-30 08:01:34 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int counter = 0;
+        int sum = 0;
+
+        for (int i = 400; i <= 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                counter++;
+                sum += i;
+                System.out.println("Found: " + i + " as match no. " + counter + ". Current sum when added is: " + sum);
+            }
+            if (counter == 5) break;
+        }
+        System.out.println("Sum of all (" + counter + ") numbers devisable by 3 and 5 is: " + sum);
     }
 }
