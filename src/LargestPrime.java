5fb2077f92b7e14a41edf63d67574080d52e59ee
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-12-02 14:10:06 +0100

    Coding exercise 26 done

---
+++ src/LargestPrime.java
@@ -0,0 +1,26 @@
+public class LargestPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(4));
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number < 2) return -1;
+
+        int largestPrime = 1;
+
+        for (int i = 2; i <= number; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+
+            if (isPrime && number % i == 0) largestPrime = i;
+        }
+
+        return largestPrime;
+    }
+}
