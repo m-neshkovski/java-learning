f9e33063d3de084758c6055172d6afb47d5ff4e8
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-12-02 09:29:01 +0100

    Coding exercise 21 done

---
+++ src/FactorPrinter.java
@@ -0,0 +1,13 @@
+public class FactorPrinter {
+    public static void main(String[] args) {
+        printFactors(1258);
+    }
+
+    public static void printFactors(int n) {
+        if (n < 0) System.out.println("Invalid Value");
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) System.out.println(i);
+        }
+    }
+}
