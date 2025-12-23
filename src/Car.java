23ecce92053ebaf17b23bb061b78edb6f06ca823
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-12-23 11:21:25 +0100

    section-7.79 done

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    // Class variables or ATTRIBUTES
+    private String make;
+    private String model;
+    private String color;
+    private int doors;
+    private boolean convertible;
+
+    public void describeCar() {
+        System.out.println(doors + "-Door " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+}
