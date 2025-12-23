b137546fecf919c4756905aab4c852278c3a8aa3
Milosh Neshkovski <m.neshkovski@gmail.com>
2025-12-23 11:38:32 +0100

    section-7.80 done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         Car car = new Car();
+        car.make = "Porsche";
+        car.model = "Carrera";
+        car.color = "Red";
+        System.out.println("Car make" + car.getMake());
+        System.out.println("Car model" + car.getModel());
         car.describeCar();
     }
 }
