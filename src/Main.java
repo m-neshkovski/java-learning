6a70495b8d792120d3c47dc2c9df17e23aa3fa93
Milosh Neshkovski <m.neshkovski@gmail.com>
2026-01-12 16:01:08 +0100

    section-7-exercise Point done

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,14 @@ public class Main {
         System.out.println("Width = " + wall.getWidth());
         System.out.println("Height = " + wall.getHeight());
         System.out.println("Area = " + wall.getArea());
+
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
     }
 }
---
+++ src/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+        this(0,0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow((x - this.x), 2) + Math.pow((y - this.y), 2));
+    }
+
+    public double distance() {
+        return distance(0,0);
+    }
+
+    public double distance(Point point) {
+        return distance(point.x, point.y);
+    }
+}
